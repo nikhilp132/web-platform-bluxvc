9305e6a4407ec1a0ce9721d11bd9b39ebedc1505
nikhilp132 <nikhilparadkar200@gmail.com>
2021-08-09 22:22:29 +0530

    1 file modified

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ var str = "Hi! I'm Nikhil";
 $('.typeBox').text(str);
 
 var i = 0;
-
 var el1 = document.getElementById('typeBox');
 var el2 = $('#typeBox');
-
+var updatedString = '';
 function typeWriter() {
   if (i < str.length) {
-    el1.innerHTML += str.charAt(i);
+    // el1.innerHTML += str.charAt(i);
+    updatedString += str.charAt(i);
     i++;
+    el2.html(updatedString);
     setTimeout(typeWriter, 100);
   }
 }
